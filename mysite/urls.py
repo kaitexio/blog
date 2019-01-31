8ffa2904a40f978cb459741c9d7b80c69bf4c2a9
kai <kaitexio0522@me.com>
2019-02-01 00:45:25 +0900

    試しにコミット

--- mysite/urls.py
+++ mysite/urls.py
@@ -37,9 +37,5 @@ urlpatterns = [
 
 ]
 
-
-
-
-
 if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
